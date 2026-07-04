8f116f3173684ab1a1c4e6c9847f4147b04fc162
Krishna Agrawal <kagrawal050@gmail.com>
2026-07-04 13:50:49 +0530

    next2

---
+++ Next/static/chunks/app/layout-380151d0663538e7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{898:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f367f3"}},2162:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,898,23)),Promise.resolve().then(l.t.bind(l,3673,23))},3673:()=>{}},e=>{e.O(0,[425,441,255,358],()=>e(e.s=2162)),_N_E=e.O()}]);
